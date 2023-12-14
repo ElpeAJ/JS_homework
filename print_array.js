a76342b9ecf37a56302094d82851e31d85fb6086
Elpedia Arthur <elpediaarthurjunior@gmail.com>
2023-12-13 23:15:46 -0800

    first Task

---
+++ print_array.js
@@ -0,0 +1,8 @@
+// Build as simple loop, that prints numbers to the console.
+// from 0 to the length of the Array.
+
+let siblings = ["Elpe", "Sonia", "Stevoo", "Glady", "Growel"];
+
+for (let sibling = 0; sibling <= siblings.length - 1; sibling++) {
+  console.log(sibling);
+}
