2546a1e14233ab7b2211f152e412b35829ea4680
Elpedia Arthur <elpediaarthurjunior@gmail.com>
2023-12-13 23:23:03 -0800

    second Task

---
+++ print_arrValue.js
@@ -0,0 +1,11 @@
+// Build a simple loop, inside a function, that takes an
+// array and prints the value of the array to the console.
+
+function printSiblings(arr) {
+  for (let sibling = 0; sibling <= siblings.length - 1; sibling++) {
+    console.log(arr[sibling]);
+  }
+}
+
+let siblings = ["Elpe", "Sonia", "Stevoo", "Glady", "Growel"];
+printSiblings(siblings);
